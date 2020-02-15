48bb83bab6f3e9b1342b92ef6dc100c06d258ce3
degrecci <henrique.degrecci@upx.com>
2020-02-15 11:19:48 -0300

    Starta validação com yup

--- react-8/src/App.js
+++ react-8/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { string } from 'yup'; // for only what you need
 
 import { Container, Heading, Section } from 'react-bulma-components';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const schema = string()
+    .required('Campo requerido')
+    .max(39, 'O máximo de caracteres é 39');
+
+  const handleSubmit = async event => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log('submit', username);
+      console.log('response', await schema.isValid(username));
+    }
+  };
+
   return (
     <Section>
       <Container>
@@ -18,6 +33,9 @@ function App() {
             type="text"
             name="user"
             placeholder="Digite um usuário do github"
+            value={username}
+            onChange={event => setUsername(event.target.value)}
+            onKeyPress={handleSubmit}
           />
         </form>
       </Container>
